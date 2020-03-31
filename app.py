6f4774bccd8f657dc5472136a1207578cc68172a
abishalom <abishalom1997@gmail.com>
2020-03-31 16:23:15 -0400

    fix heroku

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import dash_bootstrap_components as dbc
 import dash
 # external_stylesheets =  [dbc.themes.DARKLY]
 external_stylesheets=[]
-app = dash.Dash('Covid-Tracking', external_stylesheets= external_stylesheets)
+app = dash.Dash(__name__, external_stylesheets= external_stylesheets)
 server = app.server
 app.config.suppress_callback_exceptions = True
 
