625f9b9974f717bf22cf6b34c183aa6cdc442a88
abishalom <abishalom1997@gmail.com>
2020-04-22 22:37:20 -0400

    Data update 4/22

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import dash
 # external_stylesheets =  [dbc.themes.DARKLY]
 external_stylesheets=[]
 app = dash.Dash(__name__, external_stylesheets= external_stylesheets)
+app.title = "CovidTracker"
 server = app.server
 app.config.suppress_callback_exceptions = True
 
